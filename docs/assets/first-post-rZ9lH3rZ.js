48bc93092e7f7cfb84099fe0cb12b82946f046d8
Domen Koder <domen.koder@gmail.com>
2025-07-24 21:18:02 +0200

    chore: set thing up for migrating to cloudflare pages

--- docs/assets/first-post-rZ9lH3rZ.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./index-DF_YsMxi.js";const r={title:"First Post",summary:"My first blog post!",date:"2025-07-24",tags:["blogging","meta"],author:"Domen Koder"};function e(n){const o={p:"p",...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(o.p,{children:`This is my first post written in MDX!\r
-On my blog I'll be posting cool stuff I come across or learn — mostly about programming and tech. I'm open to suggestions if you have any!`}),`
-`,t.jsx("br",{}),`
-`,t.jsx(o.p,{children:":wq"}),`
-`,t.jsx(o.p,{children:"- Domen"})]})}function i(n={}){const{wrapper:o}=n.components||{};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{i as default,r as meta};
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,4 @@ import mdx from '@mdx-js/rollup';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss(), mdx()],
-	build: {
-		outDir: 'docs',
-	},
 });
