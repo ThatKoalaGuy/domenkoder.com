93ce5b6ce27a613d9d38cf2b972fee402b503e24
Domen Koder <domen.koder@gmail.com>
2025-07-24 20:51:43 +0200

    fix: forgot to build ... again ...

--- docs/assets/first-post-rZ9lH3rZ.js
+++ docs/assets/first-post-rZ9lH3rZ.js
@@ -1,4 +1,4 @@
-import{j as t}from"./index-CHBODqEn.js";const r={title:"First Post",summary:"My first blog post!",date:"2025-07-24",tags:["blogging","meta"],author:"Domen Koder"};function e(n){const o={p:"p",...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(o.p,{children:`This is my first post written in MDX!\r
+import{j as t}from"./index-DF_YsMxi.js";const r={title:"First Post",summary:"My first blog post!",date:"2025-07-24",tags:["blogging","meta"],author:"Domen Koder"};function e(n){const o={p:"p",...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(o.p,{children:`This is my first post written in MDX!\r
 On my blog I'll be posting cool stuff I come across or learn — mostly about programming and tech. I'm open to suggestions if you have any!`}),`
 `,t.jsx("br",{}),`
 `,t.jsx(o.p,{children:":wq"}),`
