e0849c58a63fbf073710cb8879581b903686f57b
Domen Koder <domen.koder@gmail.com>
2025-07-28 11:26:01 +0200

    fix: fix the homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 				name="description"
 				content="Domen Koder is a software engineering student with a passion for web development and Linux. Explore projects and connect online."
 			></meta>
-			<main className="flex justify-center items-center flex-1 p-4">
+			<main className="flex justify-center items-center min-h-[calc(87vh-5rem)] p-4">
 				<div>
 					<h1 className="text-5xl sm:text-9xl">Hi, I'm Domen.</h1>
 					<h2 className="text-2xl sm:text-5xl">
