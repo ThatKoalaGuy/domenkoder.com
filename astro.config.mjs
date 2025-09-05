fa4cc2ca436116048d27a502f07a49adc1f2d5f0
Domen Koder <domen.koder@gmail.com>
2025-09-05 18:52:14 +0200

    chore: forgot to add site for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [mdx(), sitemap()]
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	site: 'https://domenkoder.com',
+	integrations: [mdx(), sitemap()],
 });
